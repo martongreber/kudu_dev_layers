f887fe852c09db6d38b9cfb7f5c3771fb0282c21
Marton Greber <greber.mx@gmail.com>
2023-04-15 20:31:56 +0200

    refactored build_all.sh a bit

--- build_all.sh
+++ build_all.sh
@@ -1,36 +1,42 @@
+
+build_base() {
+    build_type=$1
+    layer=base
+    cd ./$layer
+    docker build --build-arg username=$github_username \
+        --build-arg parent_image=murculus/kudu-${build_type} \
+        -t $(whoami)/${layer}_${build_type}_$(uname -m):$(date +%Y%m%d) .
+    cd ../
+}
+
+build_layer() {
+    build_type=$1
+    layer=$2
+    cd ./$layer
+    docker build --build-arg parent_image=$(whoami)/base_${build_type}_$(uname -m):$(date +%Y%m%d) \
+        -t $(whoami)/${layer}_$(uname -m):$(date +%Y%m%d) .
+    cd ../
+}
+
+# For testing purpose
 github_username=martongreber
+# Most of the time for dev purposes the debug build types are just enough.
+# First build the base
 build_type=debug
+build_base $build_type
 
-layer=base
-cd ./$layer
-docker build --build-arg username=$github_username \
-    --build-arg parent_image=murculus/kudu-${build_type} \
-    -t $(whoami)/${layer}_${build_type}_$(uname -m):$(date +%Y%m%d) .
-cd ../
+# Indepent layers which only build on top of base
+independent_layers=("cpp_client_example" \
+                    "python_client")
+for layer in ${independent_layers[@]}; do
+    build_layer $build_type $layer
+done
 
-layer=cpp_client_example
-cd ./$layer
-docker build --build-arg parent_image=$(whoami)/base_${build_type}_$(uname -m):$(date +%Y%m%d) \
-    -t $(whoami)/${layer}_$(uname -m):$(date +%Y%m%d) .
-cd ../
 
-layer=python_client
-cd ./$layer
-docker build --build-arg parent_image=$(whoami)/base_${build_type}_$(uname -m):$(date +%Y%m%d) \
-    -t $(whoami)/${layer}_$(uname -m):$(date +%Y%m%d) .
-cd ../
+# Docs are built on top of release build. Takes a couple mins as the cache misses alot because of the 
+# different CMake config which is used in make_site.sh
 
-# Docs are built on top of release build.
 # build_type=release
-# layer=base
-# cd ./$layer
-# docker build --build-arg username=$github_username \
-#     --build-arg parent_image=murculus/kudu-${build_type} \
-#     -t $(whoami)/${layer}_${build_type}_$(uname -m):$(date +%Y%m%d) .
-# cd ../
-
+# build_base $build_type
 # layer=docs
-# cd ./${layer}
-# docker build --build-arg parent_image=$(whoami)/base_${build_type}_$(uname -m):$(date +%Y%m%d) \
-#     -t $(whoami)/${layer}_$(uname -m):$(date +%Y%m%d) .
-# cd ../
+# build_layer $build_type $build_layer
