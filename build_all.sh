aac228b2f9aa765ebca1ec5c30ba5be1f5168600
Marton Greber <greber.mx@gmail.com>
2023-05-24 13:35:10 +0200

    refactor build_all.sh now it accepts github username as first argument

--- build_all.sh
+++ build_all.sh
@@ -1,20 +1,41 @@
+#!/bin/bash
 
+## Arguments
+if [ -z $1 ]; then
+    echo "Please supply your github username as the first argument"
+fi
+github_username=$1
+
+
+## Docker build functions
+# base layer is handled litte bit differently
 build_base() {
     build_type=$1
     layer=base
+    # tag format: <username>/<layer_name>_<build_type>_<architecture>:<date>
+    # example: martongreber/base_debug_x86_64:20230524
+    tag=$(whoami)/${layer}_${build_type}_$(uname -m):$(date +%Y%m%d)
+
     cd ./$layer
-    docker build --build-arg username=$github_username \
+    docker build \
+        --build-arg username=$github_username \
         --build-arg parent_image=murculus/kudu-${build_type} \
-        -t $(whoami)/${layer}_${build_type}_$(uname -m):$(date +%Y%m%d) .
+        -t $tag .
     cd ../
 }
-
+# building on top of base follows the same pattern
 build_layer() {
     build_type=$1
-    layer=$2
+    # refer to the tag from build_base()
+    parent_image=$(whoami)/base_${build_type}_$(uname -m):$(date +%Y%m%d)
+    # tag format: <username>/<layer_name>_<architecture>:<date>
+    # example: martongreber/docs_x86_64:20230524
+    tag=$(whoami)/${layer}_$(uname -m):$(date +%Y%m%d)
+
     cd ./$layer
-    docker build --build-arg parent_image=$(whoami)/base_${build_type}_$(uname -m):$(date +%Y%m%d) \
-        -t $(whoami)/${layer}_$(uname -m):$(date +%Y%m%d) .
+    docker build \
+        --build-arg parent_image=$parent_image \
+        -t $tag .
     cd ../
 }
 
@@ -22,9 +43,6 @@ main() {
     DIR="$(dirname "${BASH_SOURCE[0]}")"
     cd $DIR
 
-    # For testing purpose
-    github_username=martongreber
-    # Most of the time for dev purposes the debug build types are just enough.
     # First build the base
     build_type=debug
     build_base $build_type
@@ -35,7 +53,6 @@ main() {
     for layer in ${independent_layers[@]}; do
         build_layer $build_type $layer
     done
-
 
     # Docs are built on top of release build. Takes a couple mins as the cache misses alot because of the 
     # different CMake config which is used in make_site.sh
